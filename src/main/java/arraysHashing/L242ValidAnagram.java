b31c52513be065d09bdfdce38dd1efa6f7938939
turgayserdas <turgay.serdas@gmail.com>
2023-01-09 00:35:15 +0300

    l217

---
+++ src/main/java/arraysHashing/L242ValidAnagram.java
@@ -0,0 +1,50 @@
+package arraysHashing;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+/*
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
+
+
+Constraints:
+
+1 <= s.length, t.length <= 5 * 104
+s and t consist of lowercase English letters.
+
+
+Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
+ */
+public class L242ValidAnagram {
+    public static void main(String[] args){
+        System.out.println(isAnagram("anagram","nagaram"));
+    }
+    public static boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length())
+            return false;
+        int[] charArr = new int[26];
+        for(int i=0;i<s.length();i++){
+            charArr[s.charAt(i)-'a']++;
+            charArr[t.charAt(i)-'a']--;
+        }
+        for(int x : charArr){
+            if(x!=0)
+                return false;
+        }
+        return true;
+    }
+}
